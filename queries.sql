98a58d62d060a03186bd6d1a27bca661a431dd5c
ZuzannaKochowska <132690615+ZuzannaKochowska@users.noreply.github.com>
2025-07-07 14:36:47 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -18,7 +18,7 @@ FROM Movies_clean
 ORDER BY release_date DESC
 LIMIT 10;
 
---Top 5 filmów według oceny × liczba głosów
+--Top 5 filmów według oceny × liczba głosów (ocena popularności)
 SELECT title, (vote_average*vote_count) AS score
 FROM Movies_clean
 WHERE vote_count >= 1000
@@ -39,6 +39,12 @@ FROM movies_clean
 WHERE vote_count >= 100
 ORDER BY weighted_rating DESC
 LIMIT 20;
+
+--Lista 10 najwyżej ocenianych filmów z największą liczbą głosów // rzetelniejszy ranking jest według wzoru IMDB powyżej
+SELECT * FROM movies_clean
+WHERE vote_count > 10000
+ORDER BY vote_average DESC
+LIMIT 10;
 
 3. Agregacje i grupowanie
 --Oblicz średnią ocenę filmów z każdego roku.
@@ -89,6 +95,82 @@ SELECT original_language, title, vote_average
  FROM movies_clean
  WHERE vote_average = (
 		SELECT MAX(vote_average) FROM movies_clean WHERE vote_count > 10000);
+		
+--Najstarszy film w bazie (Użyj MIN(release_date), Pokaż tytuł i datę premiery)
+SELECT title, release_date FROM Movies_clean
+WHERE release_date = (
+					SELECT MIN(release_date) FROM Movies_clean
+					);
+
+--Liczba filmów wydanych każdego roku (posortowane)->(YEAR(release_date), COUNT(*) GROUP BY + ORDER BY)
+SELECT YEAR(release_date) AS rok, COUNT(*) AS liczba_filmow FROM movies_clean
+GROUP BY rok
+ORDER BY rok ASC;
+
+--Zidentyfikować najlepiej oceniane filmy w każdym języku z wykorzystaniem MAX() w podzapytaniu i JOIN 
+SELECT m.*
+FROM movies_clean m
+JOIN (
+    SELECT original_language, MAX(vote_average) AS max_rating
+    FROM movies_clean
+    GROUP BY original_language
+) AS max_per_lang
+ON m.original_language = max_per_lang.original_language
+   AND m.vote_average = max_per_lang.max_rating
+ORDER BY m.original_language;
+
+--Średnia ocena filmów z każdego roku z GROUP BY YEAR(release_date) + AVG(vote_average)
+SELECT YEAR(release_date) AS rok, ROUND(AVG(vote_average), 2) AS srednia_ocena, COUNT(*) AS ilosc_filmow_per_rok 
+FROM movies_clean
+GROUP BY rok
+ORDER BY rok ASC;
+
+--Filmy, których tytuły zaczynają się na literę „T"
+SELECT title 
+FROM movies_clean
+WHERE title LIKE 'T%' ;
+
+
+--Języki, w których nakręcono ponad 100 filmów Z GROUP BY original_language + HAVING COUNT(*) > 100
+SELECT original_language, count(*) AS ilosc_filmow FROM movies_clean
+GROUP BY original_language 
+HAVING count(*) > 100;
+
+--Filmy bez podanego budżetu lub WHERE budget IS NULL
+SELECT * FROM movies_clean
+WHERE budget IS NULL OR budget=0;
+
+--Najdłuższy i najkrótszy tytuł filmu - CHAR_LENGTH(title) + MAX() i MIN()
+SELECT Title, CHAR_LENGTH(Title) AS dlugosc_tytulu
+FROM movies_clean
+WHERE CHAR_LENGTH(Title) = (
+    SELECT MAX(CHAR_LENGTH(Title)) FROM movies_clean
+);
+
+lub 
+SELECT  Title, Char_length(title) AS ilosc_znakow from movies_clean
+ORDER BY ilosc_znakow DESC
+LIMIT 1;
+--Liczba filmów na każdy miesiąc - MONTH(release_date) + GROUP BY
+SELECT MONTH(release_date) AS miesiac, COUNT(*) FROM movies_clean
+GROUP BY miesiac
+ORDER BY miesiac;
+
+--Średnia liczba głosów (vote_count) na film w każdym języku
+SELECT original_language, ROUND(AVG(vote_average), 4) AS srednia_ocena, COUNT(*) as liczba_glosow 
+FROM movies_clean
+GROUP BY original_language
+ORDER BY srednia_ocena DESC;
+
+
+--Filmy, które mają głosów więcej niż 10 000 i ocenę powyżej 8
+SELECT * FROM movies_clean
+WHERE vote_count > 10000 AND vote_average > 8;
+
+-- Znajdź wszystkie filmy, które mają w tytule słowo "love"
+SELECT title FROM movies_clean
+WHERE title LIKE "%love%";
+
 
 5. Modyfikacje danych
 --Zaktualizuj ocenę (vote_average) filmu o konkretnym id.
@@ -99,6 +181,8 @@ WHERE id = 36593;
 --Usuń filmy wydane przed rokiem 1998.
 DELETE FROM movies_clean
 WHERE release_date < '1998-01-01';
+
+
 
 6. Tworzenie widoków i indeksów
 Stwórz widok pokazujący filmy z oceną powyżej 8 i datą premiery po 2015.
