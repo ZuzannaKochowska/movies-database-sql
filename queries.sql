544c1c839118e7b36abd2bbea25881a5b1023714
ZuzannaKochowska <132690615+ZuzannaKochowska@users.noreply.github.com>
2025-07-12 22:26:29 +0200

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -183,17 +183,156 @@ WHERE id = 36593;
 DELETE FROM movies_clean
 WHERE release_date < '1998-01-01';
 
+6. JOINY
+--Pokaż tytuły filmów oraz nazwy kolekcji, do których należą (jeśli film należy do kolekcji).
+SELECT m.title, c.collection_name 
+FROM movies_clean m
+LEFT JOIN collections c 
+ON m.id = c.movie_id;
 
-6. Tworzenie widoków i indeksów // Views and Indexing
+--Pokaż liczbę filmów w każdej kolekcji.
+SELECT c.collection_name,  COUNT(*) AS ilosc_filmow_w_kolekcji
+FROM collections c
+LEFT JOIN movies_clean m
+ON c.movie_id = m.id
+GROUP BY collection_name 
+ORDER BY ilosc_filmow_w_kolekcji DESC;
+
+--Pokaż 10 najnowszych filmów z gatunku „Action” 
+SELECT m.title, m.release_date, g.genre_name 
+FROM movies_clean m 
+JOIN genres g
+ON m.id = g.movie_id
+WHERE g.genre_name = 'Action'
+ORDER BY release_date DESC
+LIMIT 10;
+
+--Pokaż filmy, które mają większy budżet niż średni budżet wszystkich filmów
+SELECT title, budget FROM movies_clean
+WHERE budget > (
+				SELECT AVG(budget) FROM movies_clean
+                );
+
+
+--Pokaż filmy z więcej niż jednym gatunkiem przypisanym
+SELECT  m.title, COUNT(DISTINCT g.genre_name) AS liczba_gatunkow FROM movies_clean m
+JOIN genres g
+ON m.id = g.movie_id
+GROUP BY m.title
+HAVING liczba_gatunkow > 1
+ORDER BY liczba_gatunkow DESC;
+
+--Dla każdego gatunku pokaż średni budżet filmów
+
+SELECT g.genre_name, AVG(budget)
+FROM movies_clean m 
+JOIN genres g 
+ON m.id = g.movie_id
+GROUP BY g.genre_name;
+
+--Wyświetl tytuł filmu i imiona aktorów, którzy zagrali w danym filmie.
+SELECT c.name, m.title FROM cast_clean c 
+LEFT JOIN  movies_clean m 
+ON m.id = c.movie_id;
+
+
+--Pokaż tytuły filmów i reżysera.
+SELECT m.title,  c.name AS 'Director' FROM movies_clean m 
+LEFT JOIN  crew_clean c
+ON m.id = c.movie_id
+WHERE c.job = 'Director';
+
+--Wyświetl tytuły filmów, które należą do jakiejś kolekcji wraz z nazwą tej kolekcji.
+SELECT c.collection_name, m.title FROM movies_clean m
+JOIN collections c 
+ON m.id = c.movie_id;
+
+--Pokaż aktorów, którzy wystąpili w więcej niż 3 filmach.
+SELECT name, COUNT(movie_id) AS ilosc_filmow FROM cast_clean
+GROUP BY name
+HAVING ilosc_filmow >= 3
+ORDER BY ilosc_filmow DESC;
+
+--Pokaż tytuły filmów i liczbę osób z ekipy (crew) przypisanych do każdego filmu.
+SELECT m.title, COUNT(c.movie_id) AS amount_of_crew FROM movies_clean m
+JOIN crew_clean c 
+ON m.id = c.movie_id
+GROUP by m.title;
+
+--Wyświetl 10 najczęstszych gatunków filmowych i ile filmów do nich należy.
+SELECT genre_name, COUNT(genre_name) AS ilosc 
+FROM genres
+GROUP BY genre_name
+ORDER BY ilosc DESC
+LIMIT 10;
+
+--Pokaż listę filmów z gatunku "Comedy", które mają więcej niż 7.5 średniej oceny
+SELECT m.title, m.vote_average FROM 
+movies_clean m
+JOIN genres g 
+ON m.id = g.movie_id 
+WHERE g.genre_name = 'Comedy'
+AND m.vote_average >= 7.5
+ORDER BY m.vote_average DESC;
+
+--Wyświetl aktorów, którzy wystąpili razem w więcej niż jednym filmie.
+SELECT 
+	c1.name AS actor1,
+    c2.name AS actor2,
+    COUNT(*) AS wspolne_filmy   
+FROM cast_clean c1 
+JOIN cast_clean c2
+ON c1.movie_id = c2.movie_id
+WHERE c1.name < c2.name
+GROUP BY c1.name, c2.name
+HAVING COUNT(*) > 1
+ORDER BY wspolne_filmy DESC
+LIMIT 10;
+
+--Pokaż filmy, których budżet jest wyższy niż średni budżet filmów w tej samej kolekcji.
+SELECT m.title, m.budget 
+FROM movies_clean m 
+WHERE m.budget > (
+	SELECT AVG(m2.budget)
+	FROM movies_clean m2
+	JOIN collections c 
+    ON m2.id = c.movie_id
+);
+
+--Wyświetl imię i zawód (job) każdej osoby z crew, która pracowała przy filmach w gatunku „Drama”.
+SELECT c.name, c.job 
+FROM crew_clean c 
+JOIN genres g 
+ON c.movie_id = g.movie_id
+WHERE g.genre_name = 'Drama';
+
+-- Tworzenie widoków i indeksów // Views and Indexing
 Stwórz widok pokazujący filmy z oceną powyżej 8 i datą premiery po 2015. // Create a view showing movies with a rating above 8 and release date after 2015.
+CREATE VIEW top_movies_2015 AS
+SELECT title, release_date, vote_average
+FROM movies_clean
+WHERE vote_average > 8 AND release_date > '2015-01-01';
 
-Dodaj indeks na kolumnę original_language dla szybszego filtrowania. // Add an index to the original_language column to improve performance.
+--Dodaj indeks na kolumnę original_language dla szybszego filtrowania. // Add an index to the original_language column to improve performance.
+CREATE INDEX idx_original_language
+ON movies_clean(original_language);
 
-7. Transakcje // Transactions
 
-Wykonaj serię zmian (np. aktualizacja kilku filmów), a potem zrób rollback, by ćwiczyć transakcje. // Execute multiple updates and test the rollback functionality.
+--Transakcje // Transactions
+--Wykonaj serię zmian (np. aktualizacja kilku filmów), a potem zrób rollback, by ćwiczyć transakcje. // Execute multiple updates and test the rollback functionality.
+START TRANSACTION;
 
-8. Eksport i import // Export and Import
-Wyeksportuj wybrane dane do pliku CSV.// Export selected data to a CSV file.
+UPDATE movies_clean
+SET vote_average = vote_average + 1
+WHERE vote_count > 10000;
 
-Zaimportuj zmodyfikowany plik z powrotem do bazy.//Re-import modified data back into the database.
+UPDATE movies_clean
+SET budget = budget * 2
+WHERE release_date < '2000-01-01';
+
+ROLLBACK;
+
+--Eksport i import // Export and Import
+--Wyeksportuj wybrane dane do pliku CSV.// Export selected data to a CSV file.
+
+
